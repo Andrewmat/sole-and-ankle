ff6ac6565b69aefd417ce681325da666bac37126
andre.matulionis <andre.matulionis@ifood.com.br>
2022-12-08 14:18:22 -0300

    fix next warn viewport metatag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '~root/styles/globals.css'
 
 export default function App(props: AppProps) {
-	return <props.Component {...props.pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+			</Head>
+			<props.Component {...props.pageProps} />
+		</>
+	)
 }
